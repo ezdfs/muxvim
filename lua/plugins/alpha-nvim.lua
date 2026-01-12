ef215b0530d26fefa465c9381e294f82d7768db4
ezdfs <ezequieldiasdev@gmail.com>
2026-01-12 09:12:48 -0300

    add nvim-alpha plugin

---
+++ lua/plugins/alpha-nvim.lua
@@ -0,0 +1,15 @@
+return {
+    'goolord/alpha-nvim',
+    config = function ()
+        local dashboard = require('alpha.themes.dashboard')
+
+        dashboard.section.header.val = {
+            [[ _______               ___ ___ __           ]],
+            [[|   |   |.--.--.--.--.|   |   |__|.--------.]],
+            [[|       ||  |  |_   _||   |   |  ||        |]],
+            [[|__|_|__||_____|__.__| \_____/|__||__|__|__|]],
+        }
+
+        require('alpha').setup(dashboard.config)
+    end
+};
