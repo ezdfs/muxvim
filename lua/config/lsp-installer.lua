47ab3ac0eda443df0598133632c1a6429cf84442
ezdfs <ezequieldiasdev@gmail.com>
2026-01-12 19:00:45 -0300

    wip: add to enable lsp

--- lua/config/lsp-installer.lua
+++ lua/config/lsp-installer.lua
@@ -4,6 +4,8 @@ local M = {}
 
 M.tools = {
 	{
+		filetype = "lua",
+		lsp_id = "lua_ls",
 		name = "Lua-LS",
 		install_cmd = "pkg install -y lua-language-server",
 		bin = "lua-language-server",
@@ -13,6 +15,8 @@ M.tools = {
 		end,
 	},
 	{
+		filetype = "python",
+		lsp_id = "pyright",
 		name = "Pyright",
 		install_cmd = "pkg install -y nodejs && npm install -g pyright",
 		bin = "pyright",
@@ -24,6 +28,63 @@ M.tools = {
 }
 
 M.setup = function()
+	local function monitor_tools()
+		local group = vim.api.nvim_create_augroup("LSPManager", { clear = true })
+		local supported_fts = {}
+
+		for _, tool in ipairs(M.tools) do
+			supported_fts[tool.filetype] = true -- Map supported fts
+
+			if vim.fn.executable(tool.bin) == 1 then
+				if tool.lsp_id then
+					vim.lsp.enable(tool.lsp_id)
+				end
+			else
+				vim.api.nvim_create_autocmd("FileType", {
+					group = group,
+					pattern = tool.filetype,
+					callback = function()
+						vim.notify(
+							"💡 LSP " .. tool.name .. " isn't installed. Use <leader>ll to install.",
+							vim.log.levels.WARN,
+							{ title = "LSP Manager" }
+						)
+					end,
+					once = true,
+				})
+			end
+		end
+
+		-- Global check for unsupported languages
+		vim.api.nvim_create_autocmd("FileType", {
+			group = group,
+			callback = function(args)
+				local ft = vim.bo[args.buf].filetype
+				-- Ignore common non-programming fts
+				local ignore = {
+					[""] = true,
+					["help"] = true,
+					["checkhealth"] = true,
+					["telescope"] = true,
+					["NvimTree"] = true,
+					["notify"] = true,
+					["lazy"] = true,
+				}
+
+				if not supported_fts[ft] and not ignore[ft] then
+					vim.notify(
+						"🚫 Language '" .. ft .. "' is not yet supported by installer.",
+						vim.log.levels.INFO,
+						{ title = "LSP Manager" }
+					)
+				end
+			end,
+			once = true,
+		})
+	end
+
+	monitor_tools()
+
 	vim.keymap.set("n", "<leader>ll", function()
 		installer.open("LSP", M.tools)
 	end, { desc = "Install LSPs" })
