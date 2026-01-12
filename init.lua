18ca9bade3051891543206b5470f637a269c848c
ezdfs <ezequieldiasdev@gmail.com>
2026-01-12 16:23:16 -0300

    refactor: organize initialization order

--- init.lua
+++ init.lua
@@ -1,9 +1,13 @@
-require("config.lazy")
-require("config.lsp").setup()
-require("config.keymaps")
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+require("config.leader")
 require("config.options")
-require("config.setup")
-require("config.cmd")
 require("config.ui")
+require("config.lazy")
 require("config.lsp-installer").setup()
 require("config.formatter-installer").setup()
+require("config.lsp").setup()
+require("config.setup")
+require("config.cmd")
+require("config.keymaps")
