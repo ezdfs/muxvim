70864cb743659ee38e6b448756af94277c7e871b
ezdfs <ezequieldiasdev@gmail.com>
2026-01-12 22:04:47 -0300

    wpi: create autocommand to load alpha screen in the first that lazy vim initializes

--- lua/config/cmd.lua
+++ lua/config/cmd.lua
@@ -1,39 +1,53 @@
+-- Open the nvim alpha screen after of Lazy
+-- started first time
+vim.api.nvim_create_autocmd("User", {
+    pattern = "LazyVimStarted",
+    callback = function()
+        if vim.fn.argc() == 0 and vim.api.nvim_buf_get_name(0) == "" then
+            local present, alpha = pcall(require, "alpha")
+            if present then
+                alpha.start(false)
+            end
+        end
+    end,
+})
+
 -- Enable colorscheme as tokyonight
 vim.cmd([[colorscheme tokyonight]])
 
 -- Configures treesitter
 vim.api.nvim_create_autocmd("FileType", {
-	callback = function(args)
-		local bufnr = args.buf
-		local ft = vim.bo[bufnr].filetype
+    callback = function(args)
+        local bufnr = args.buf
+        local ft = vim.bo[bufnr].filetype
 
-		-- Verify if exists a installed parser to this  filetype
-		local lang = vim.treesitter.language.get_lang(ft) or ft
-		local has_parser = pcall(vim.treesitter.get_parser, bufnr, lang)
+        -- Verify if exists a installed parser to this  filetype
+        local lang = vim.treesitter.language.get_lang(ft) or ft
+        local has_parser = pcall(vim.treesitter.get_parser, bufnr, lang)
 
-		if has_parser then
-			-- Enable highlighting
-			vim.treesitter.start(bufnr, lang)
+        if has_parser then
+            -- Enable highlighting
+            vim.treesitter.start(bufnr, lang)
 
-			-- Configures local buffer to this file
-			vim.opt_local.foldmethod = "expr"
-			vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-			vim.opt_local.foldlevel = 99
-		end
-	end,
+            -- Configures local buffer to this file
+            vim.opt_local.foldmethod = "expr"
+            vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+            vim.opt_local.foldlevel = 99
+        end
+    end,
 })
 
 -- Open error popup when the cursor stop on line error
 vim.api.nvim_create_autocmd("CursorHold", {
-	callback = function()
-		vim.diagnostic.open_float(nil, { focusable = false })
-	end,
+    callback = function()
+        vim.diagnostic.open_float(nil, { focusable = false })
+    end,
 })
 
 -- Configures autoformat on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function(args)
-		require("conform").format({ bufnr = args.buf })
-	end,
+    pattern = "*",
+    callback = function(args)
+        require("conform").format({ bufnr = args.buf })
+    end,
 })
