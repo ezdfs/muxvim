0c7b1f283ab2dcbf0498878ea7eadea1a947b157
ezdfs <ezequieldiasdev@gmail.com>
2026-01-13 19:33:45 -0300

    prevents bufferline works when the alpha dashboard is open

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,6 +1,27 @@
 return {
-	"akinsho/bufferline.nvim",
-	version = "*",
-	dependencies = "nvim-tree/nvim-web-devicons",
-	opts = {},
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    opts = {},
+    init = function()
+        local augroup = vim.api.nvim_create_augroup("AlphaNoBufferline", { clear = true })
+
+        vim.api.nvim_create_autocmd("FileType", {
+            group = augroup,
+            pattern = "alpha",
+            callback = function()
+                vim.opt.showtabline = 0
+            end,
+        })
+
+        vim.api.nvim_create_autocmd("BufLeave", {
+            group = augroup,
+            pattern = "*",
+            callback = function()
+                if vim.bo.filetype == "alpha" then
+                    vim.opt.showtabline = 2
+                end
+            end,
+        })
+    end,
 }
