69f38a3874303b5bd32350517557ed0cfa0ac09f
ezdfs <ezequieldiasdev@gmail.com>
2026-01-13 18:44:33 -0300

    load alpha dashboard after neovim install plugins and is started

--- lua/config/cmd.lua
+++ lua/config/cmd.lua
@@ -1,6 +1,29 @@
 -- Enable colorscheme as tokyonight
 vim.cmd([[colorscheme tokyonight]])
 
+vim.api.nvim_create_autocmd("User", {
+    pattern = "LazyVimStarted",
+    callback = function()
+        if vim.fn.argc() == 0 then
+            -- Verify if Lazy is open
+            local lazy_opened = false
+            for _, win in ipairs(vim.api.nvim_list_wins()) do
+                if vim.api.nvim_win_get_config(win).relative ~= "" then
+                    lazy_opened = true
+                    break
+                end
+            end
+
+            if lazy_opened then
+                -- "Restart" neovim
+                vim.cmd("silent! bufdo bwipeout!") -- Clear buffers
+                vim.cmd("source $MYVIMRC")         -- Reload configuration
+                vim.cmd("Alpha")                   -- Open alpha dashboard
+            end
+        end
+    end,
+})
+
 -- Configures treesitter
 vim.api.nvim_create_autocmd("FileType", {
     callback = function(args)
