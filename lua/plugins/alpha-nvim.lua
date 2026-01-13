0bffc517261e224cb97f59b662f61e68e1f7d0ba
ezdfs <ezequieldiasdev@gmail.com>
2026-01-13 17:58:12 -0300

    wip: delete autocommand to try another ways

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -11,19 +11,5 @@ return {
         }
 
         require('alpha').setup(dashboard.config)
-
-        local augroup = vim.api.nvim_create_augroup("AlphaOnEmpty", { clear = true })
-        vim.api.nvim_create_autocmd("User", {
-            group = augroup,
-            pattern = "LazyVimStarted",
-            callback = function()
-                if vim.fn.argc() == 0 and vim.api.nvim_buf_get_name(0) == "" then
-                    local present, alpha = pcall(require, "alpha")
-                    if present then
-                        alpha.start(true)
-                    end
-                end
-            end,
-        })
     end
 };
