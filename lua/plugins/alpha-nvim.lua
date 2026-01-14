40b4895ec5172f096bc8155a413bb9b779928669
ezdfs <ezequieldiasdev@gmail.com>
2026-01-13 22:49:01 -0300

    refactor: moves alpha-nvim autocommands from config/cmd.lua to plugins/alpha-nvim.lua

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -11,5 +11,31 @@ return {
         }
 
         require('alpha').setup(dashboard.config)
+
+        local group = vim.api.nvim_create_augroup("AlphaNvim", { clear = true })
+
+        vim.api.nvim_create_autocmd("User", {
+            group = group,
+            pattern = "LazyVimStarted",
+            callback = function()
+                if vim.fn.argc() == 0 then
+                    -- Verify if Lazy is open
+                    local lazy_opened = false
+                    for _, win in ipairs(vim.api.nvim_list_wins()) do
+                        if vim.api.nvim_win_get_config(win).relative ~= "" then
+                            lazy_opened = true
+                            break
+                        end
+                    end
+
+                    if lazy_opened then
+                        -- "Restart" neovim
+                        vim.cmd("silent! bufdo bwipeout!") -- Clear buffers
+                        vim.cmd("source $MYVIMRC")         -- Reload configuration
+                        vim.cmd("Alpha")                   -- Open alpha dashboard
+                    end
+                end
+            end,
+        })
     end
 };
