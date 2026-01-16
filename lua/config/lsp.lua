4843939b29ebe6447fc15185d293fea3cb4f80c7
ezdfs <ezequieldiasdev@gmail.com>
2026-01-15 22:04:59 -0300

    feat(LSP): add rename symbol autocommand and keymap

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,3 +1,15 @@
+-- Group to lsp autocommand
+local group = vim.api.nvim_create_augroup("UserLspConfig", { clear = true })
+
+-- Enable rename symbol
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = group,
+    callback = function(ev)
+        local opts = { buffer = ev.buf, desc = "Rename symbol" }
+        vim.keymap.set("n", "<space>cr", vim.lsp.buf.rename, opts)
+    end,
+})
+
 -- Enable lsp hints
 vim.lsp.inlay_hint.enable(true)
 
